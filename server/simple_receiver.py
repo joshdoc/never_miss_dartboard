b6d1ac67568c6e15f1c57e1bf8a29660e2b26d76
bchurley <157984152+bchurley@users.noreply.github.com>
2025-04-14 10:06:12 -0400

    Delete server/simple_receiver.py

--- server/simple_receiver.py
+++
@@ -1,9 +0,0 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.bind(('', 4500))
-print("Listening on port 4500...")
-
-while True:
-    data, addr = s.recvfrom(1024)
-    print(f"Received {len(data)} bytes from {addr}: {data}")
